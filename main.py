b43ba49f68fbceeac66d4710898c1571e5744ed5
heliamazinani <13helia1383@gmail.com>
2025-01-21 21:39:53 +0330

    mainmenu added

--- main.py
+++ main.py
@@ -8,15 +8,76 @@ from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.lang import Builder
 Builder.load_file('ban.kv')
 
-class PageLayoutex(PageLayout):
-    pass
+class MainMenu(Screen):
+    button_1 = ["",""]
+    button_2 = ["   ","transfer"]
+    button_3 =["    ","deposit"]
+    button_4 = ["no","back"]
+    button_5 = ["   ","balance"]
+    button_6 = ["   ","list of transactions"]
+    button_7 = ["   ","withdraw"]
+    button_8 = ["yes","exit"]
+    text1 = StringProperty("    ")
+    text2 = StringProperty("transfer")
+    text3 = StringProperty("deposit")
+    text4 = StringProperty("back")
+    text5 = StringProperty("balance")
+    text6 = StringProperty("list of transactions")
+    text7 = StringProperty("withdraw")
+    text8 = StringProperty("exit")
+    def on_button_1(self,widget):            
+        print ("clicked 1")
+    def on_button_2(self,widget):
+        print ("clicked 2")
+    def on_button_3(self,widget):
+        print ("clicked 3")
+    def on_button_4(self,widget):
+        self.manager.current = 'rootpage'
+        print ("clicked 4")
+    def on_button_5(self,widget):
+        print ("clicked 5")
+    def on_button_6(self,widget):
+        print ("clicked 6")
+    def on_button_7(self,widget):
+        print ("clicked 7")
+    def on_button_8(self,widget):
+        print ("clicked 8")
 
 class rootpage(Screen):
-    def on_button(self):
-        print ("clicked")
-
-
-
+    button_1 = ["",""]
+    button_2 = ["   ","transfer"]
+    button_3 =["    ","deposit"]
+    button_4 = ["no","back"]
+    button_5 = ["   ","balance"]
+    button_6 = ["   ","list of transactions"]
+    button_7 = ["   ","withdraw"]
+    button_8 = ["yes","exit"]
+    text1 = StringProperty("    ")
+    text2 = StringProperty("    ")
+    text3 = StringProperty("    ")
+    text4 = StringProperty("no")
+    text5 = StringProperty("    ")
+    text6 = StringProperty("    ")
+    text7 = StringProperty("    ")
+    text8 = StringProperty("yes")
+    def on_button_1(self,widget):            
+        print ("clicked 1")
+    def on_button_2(self,widget):
+        print ("clicked 2")
+    def on_button_3(self,widget):
+        print ("clicked 3")
+    def on_button_4(self,widget):
+        self.manager.current = 'mainmenu'
+        print ("clicked 4")
+    def on_button_5(self,widget):
+        print ("clicked 5")
+    def on_button_6(self,widget):
+        print ("clicked 6")
+    def on_button_7(self,widget):
+        print ("clicked 7")
+    def on_button_8(self,widget):
+        self.manager.current = 'mw'
+        print ("clicked 8")
 class MainWidget(Screen):
     my_text = StringProperty("1")
     text_input = StringProperty("enter")
@@ -42,8 +103,9 @@ class BankApp(App):
         sm = ScreenManager(transition=FadeTransition())
         sm.add_widget(rootpage(name='rootpage'))
         sm.add_widget(MainWidget(name='mw'))
+        sm.add_widget(MainMenu(name='mainmenu'))
 
         return sm
 
-
+state = "auth"
 BankApp().run()
