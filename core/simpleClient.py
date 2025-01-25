e15cfd093ce4de69f1e7ef08a41fc3eba35ef5e4
anormalprgrmr <abolfazl.amm.hcb@gmail.com>
2025-01-25 17:55:27 +0330

    without atomic transaction

--- core/simpleClient.py
+++ core/simpleClient.py
@@ -6,7 +6,7 @@ RESPONSE_PIPE = "/tmp/core_response_pipe"
 
 def send_request(cardNumber, action, value=0):
     request = json.dumps([cardNumber, action, value])
-
+    print("request is : ",request)
     with open(REQUEST_PIPE, "w") as req_pipe:
         req_pipe.write(request)
 
@@ -15,11 +15,37 @@ def send_request(cardNumber, action, value=0):
         print(f"Response: {response}")
 
 if __name__ == "__main__":
-    send_request(1001, "deposit", 2)
-    send_request(1001, "withdraw", 3)
-    send_request(1001, "withdraw", 10)
-    send_request(1002, "withdraw", 5)
-    send_request(1003, "deposit", 5)
-
+    
+    send_request(1002,'transfer#1003',100)
+    send_request(1003,'transfer#1004',100)
+    send_request(1004,'transfer#1005',100)
+    send_request(1005,'transfer#1002',100)
+    # send_request(1004,'transfer#1002',100)
+    # send_request(1004,'transfer#1002',100)
+    
+    
+    
+    # send_request(1005,'createAccount',190)
+    # send_request(1004,'transfer#1002',100)
+    # send_request(1004, "deposit", 110)
+    # send_request(1002, "withdraw", 50)
+    # send_request(1001, "withdraw", 10)
+    # send_request(1002, "withdraw", 5)
+    # send_request(1004, "withdraw", 500)
+    # send_request(1003, "deposit", 5)
+    # send_request(1004,'transfer#1002',100)
+    # send_request(1004, "deposit", 110)
+    # send_request(1002, "withdraw", 50)
+    # send_request(1005, "withdraw", 10)
+    # send_request(1002, "withdraw", 5)
+    # send_request(1004, "withdraw", 500)
+    # send_request(1005, "deposit", 5)
+    # send_request(1005,'transfer#1002',100)
+    # send_request(1004, "deposit", 110)
+    # send_request(1002, "withdraw", 50)
+    # send_request(1001, "withdraw", 10)
+    # send_request(1002, "withdraw", 5)
+    # send_request(1005, "withdraw", 500)
+    # send_request(1003, "deposit", 5)
     with open(REQUEST_PIPE, "w") as req_pipe:
         req_pipe.write("exit")
