004c52405c38ef9c1401efcb7a4f1eb5090450b6
heliamazinani <13helia1383@gmail.com>
2025-01-26 10:33:39 +0330

    2

--- GUI/controller.py
+++ GUI/controller.py
@@ -1,3 +1,4 @@
+from core.simpleClient import *
 def check_account(cardnumber):
     return True
 def check_password(password):
@@ -5,8 +6,10 @@ def check_password(password):
 def balence(cardnumber,password):
     return 0
 def deposite(cardnumber,password,value):
+    send_request(cardnumber,"deposit",value)
     return "success"
 def withdraw(cardnumber,password,value):
+    send_request(cardnumber,"withdraw",value)
     return "success"
 def transfer(cardnumber,password,value,second_cardnumber):
     return True
